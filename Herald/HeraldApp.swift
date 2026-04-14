8062c1069d30e904f4bb35c69ce4b46fb8bee132
Marcus Schmidt <marcus.schmidt@extendaretail.com>
2026-04-14 18:21:31 +0200

    Initial Commit

---
+++ Herald/HeraldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeraldApp.swift
+//  Herald
+//
+//  Created by Marcus Schmidt on 2026-04-14.
+//
+
+import SwiftUI
+
+@main
+struct HeraldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
